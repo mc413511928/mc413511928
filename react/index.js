50a50f985c86b737abdfcacd97384ad42c8a75ca
mc413511928 <413511928@qq.com>
2020-04-17 16:29:57 +0800

    花里胡哨

--- react/index.js
+++ react/index.js
@@ -1,8 +1,20 @@
 const template = require('art-template');
 const path = require('path');
-let views = path.join(__dirname, 'views', 'index.art')
+let views = path.join(__dirname, 'views', '01.art')
 let html = template(views, {
+    // user: [{
+    //         age: 18,
+    //         name: '张三',
+    //         sex: '男'
+    //     },
+    //     {
+    //         age: 19,
+    //         name: '张四',
+    //         sex: '男'
+    //     }
+    // ]
     name: '张三',
-    age: 15
+    age: 18,
+    content: '<h1>我是标签体</h1>'
 });
 console.log(html);
